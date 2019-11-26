5da287e4d728175aaf57cc0e1fcf80728e7b694b
Abdelrahmam Salem <abubakir1997@gmail.com>
2019-11-25 21:17:18 -0500

    Fixing issue with pathToRegexp

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     testPathIgnorePatterns: ['/node_modules/'],
     setupFiles: ['./test/setup.ts'],
-    snapshotSerializers: ['enzyme-to-json/serializer'],
     moduleNameMapper: {
         '^src(.*)$': '<rootDir>/src$1',
     },
--- src/isMatch.js
+++ src/isMatch.js
@@ -1,11 +1,11 @@
-import { pathToRegexp } from 'path-to-regexp'
+import * as pathToRegexp from 'path-to-regexp'
 
 export default function isMatch(compiledPathname, sourcePathname) {
     if (sourcePathname === null) {
         return false
     }
 
-    const routeRegEx = pathToRegexp(sourcePathname)
+    const routeRegEx = (pathToRegexp.pathToRegexp || pathToRegexp.default)(sourcePathname)
 
     return !!compiledPathname.match(routeRegEx)
 }
