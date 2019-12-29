aa0b8539093fec5efeda5245276225c50a5b6660
Abdelrahmam Salem <abubakir1997@gmail.com>
2019-12-29 15:45:49 -0500

    Small updates

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     testPathIgnorePatterns: ['/node_modules/'],
-    setupFiles: ['./test/setup.ts'],
     moduleNameMapper: {
         '^src(.*)$': '<rootDir>/src$1',
     },
---
+++ test/index.spec.ts
@@ -0,0 +1,8 @@
+describe('@anew/router', () => {
+    test('test_name', () => {
+        const actual = true
+        const expected = true
+
+        expect(actual).toBe(expected)
+    })
+})
